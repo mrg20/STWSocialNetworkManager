b435596ff2007e97385c1ef45da1ec33d77e1e97
Marc Ribalta Gene <mariva1995@gmail.com>
2017-04-14 21:21:51 +0200

    added a temporal account profile

--- STWProject/SocialNetworkManagerApp/views.py
+++ STWProject/SocialNetworkManagerApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def profile_helloworld(request):
+    return HttpResponse("You are now logged in (this is not permanent)")
