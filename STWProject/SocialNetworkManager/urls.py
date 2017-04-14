5f4b0147d338907fabd7480a81d07d6b494f571b
Marc Ribalta Gene <mariva1995@gmail.com>
2017-04-14 21:21:30 +0200

    added a temporal account profile

--- STWProject/SocialNetworkManager/urls.py
+++ STWProject/SocialNetworkManager/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
+from SocialNetworkManagerApp.views import profile_helloworld
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$',  login,  name='login'),
     url(r'^accounts/logout/$',  logout,  name='logout'),
+    url(r'^accounts/profile/$', profile_helloworld)
 ]
