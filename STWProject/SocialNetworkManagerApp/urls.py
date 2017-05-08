c1b391fa18b6c6004fae485b4a17374e9899edf6
Alba Lamas <alba.v.lmas@gmail.com>
2017-05-08 22:52:16 +0200

    commit changes 4 pull

--- STWProject/SocialNetworkManagerApp/urls.py
+++ STWProject/SocialNetworkManagerApp/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from django.http import request
 from django.views.generic import ListView, DetailView, CreateView
+
+from views import CreateHomepage
 from models import *
 
 urlpatterns = [
 
     url(r'^',
-        login_required(ListView.as_view(
+        login_required(CreateHomepage.as_view(
             queryset=Box.objects.filter(box_num=1),
             context_object_name='box_list',
             template_name='homepage.html'
--- STWProject/SocialNetworkManagerApp/views.py
+++ STWProject/SocialNetworkManagerApp/views.py
@@ -6,6 +6,21 @@ from django.http import HttpResponse
 from django.template.context_processors import csrf
 from django.views.generic import ListView
 
+from models import Box
+
+
+class CreateHomepage(ListView):
+    model = Box
+    template_name = 'homepage.html'
+
+
+    #query set here???
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(CreateHomepage, self).form_valid(form)
+    def logged_user(self):
+        return self.request.user
 
 def profile_helloworld(request):
     return HttpResponse("You are now logged in (this is not permanent)")
