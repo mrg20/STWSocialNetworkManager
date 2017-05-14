254fea3d712d37a1aa19b558ef0955783a5f00c7
Alba Lamas <alba.v.lmas@gmail.com>
2017-05-14 13:11:56 +0200

    button of new box created and one change in models.py for automatic numbers in Box model

--- SocialNetworkManagerApp/models.py
+++ SocialNetworkManagerApp/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Network(models.Model):
@@ -24,13 +25,16 @@ class Complement(models.Model):
 
 class Box(models.Model):
     user = models.ForeignKey(User, default=1)
-    box_num = models.IntegerField(null=False)
+    box_num = models.IntegerField(primary_key=True)
     network = models.ForeignKey(Network, null=False)
     complement = models.ForeignKey(Complement, null=False)
     logged_into_network = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.user.username + " Box: " + str(self.box_num)
+
+    def get_absolute_url(self):
+        return reverse('SocialNetworkManagerApp:box', kwargs={'pk': self.pk})
 
 
 class UserNetworkInfo(models.Model):
--- SocialNetworkManagerApp/urls.py
+++ SocialNetworkManagerApp/urls.py
@@ -10,8 +10,16 @@ urlpatterns = [
         login_required(ShowAllBox.as_view()),
         name='homepage'),
 
+    url(r'^homepage/',
+        login_required(ShowAllBox.as_view()),
+        name='homepage'),
+
     url(r'^box/(?P<pk>\d+)/$',
         login_required(ShowSingleBox.as_view()),
-        name='box_detail')
+        name='box_detail'),
+
+    url(r'^new_box/$',
+        login_required(BoxCreate.as_view(model=Box, success_url="/")),
+        name='new_box')
 
 ]
