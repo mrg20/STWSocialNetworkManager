f626c2cfc8123704259df28d239764aa45687508
Marc Ribalta Gene <mariva1995@gmail.com>
2017-05-22 12:35:00 +0200

    added some things

--- features/steps/show_box.py
+++ features/steps/show_box.py
@@ -5,19 +5,22 @@ use_step_matcher("re")
 
 @given("I am logged in as")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    pass
+    from django.contrib.auth.models import User
+    user_info = []
+    for row in context.table:
+        for heading in row.headings:
+            user_info = row[heading]
+    User.objects.create(username=user_info[0], password=user_info[1])
 
 
 @step("I have a registered box")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    pass
-
+    from SocialNetworkManagerApp.models import Network
+    from SocialNetworkManagerApp.models import Complement
+    from SocialNetworkManagerApp.models import Box
+    Network.objects.create()
+    Complement.objects.create()
+    Box.objects.create()
 
 @then("I want to see the information of it")
 def step_impl(context):
