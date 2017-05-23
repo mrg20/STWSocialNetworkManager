123c72cfd9159a7fac53516ea087b505218433ee
Marc Ribalta Gene <mariva1995@gmail.com>
2017-05-23 21:44:51 +0200

    deleted the math library and added some values in the templates

--- SocialNetworkManager/settings.py
+++ SocialNetworkManager/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'SocialNetworkManagerApp',
-    'mathfilters'
+    'SocialNetworkManagerApp'
 ]
 
 MIDDLEWARE_CLASSES = [
